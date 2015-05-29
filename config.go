c7a93c79f7a7b1e35c8128f5d560daf3bb943b64
Marco Priebe <marco.priebe@kaeuferportal.de>
2015-05-29 15:30:37 +0200

    change read config

--- config.go
+++ config.go
@@ -2,30 +2,15 @@ package main
 
 import (
 	"gotank/libs/yaml"
-	"gotank/modules"
 	"io/ioutil"
 	"log"
 )
-
-type config struct{}
 
 type mainConfig struct {
 	Modules []string
 }
 
-func initModules() {
-	m := mainConfig{}
-	c := config{}
-	c.readConfig("./config/modules.yml", &m)
-
-	for _, value := range m.Modules {
-		i := make(map[interface{}]interface{})
-		c.readConfig("./config/"+value+".yml", &i)
-		modules.InitModule(value, i)
-	}
-}
-
-func (c config) readConfig(filename string, data interface{}) {
+func readConfig(filename string, data interface{}) {
 	b, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatalf("error: %v", err)
--- modules.go
+++ modules.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"gotank/modules"
 	"gotank/modules/motorshield"
 	"gotank/modules/ultrasonic"
 )
@@ -8,4 +9,14 @@ import (
 func registerModules() {
 	motorshield.Register()
 	ultrasonic.Register()
+}
+
+func initModules() {
+	m := mainConfig{}
+	readConfig("./config/modules.yml", &m)
+	for _, value := range m.Modules {
+		i := make(map[interface{}]interface{})
+		readConfig("./config/"+value+".yml", &i)
+		modules.InitModule(value, i)
+	}
 }

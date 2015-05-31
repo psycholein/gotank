44328cd3a11dfdcd011e865dde337b897814f005
psycho <priebeman@googlemail.com>
2015-05-31 09:44:31 +0200

    add distance messure

--- modules/ultrasonic/ultrasonic.go
+++ modules/ultrasonic/ultrasonic.go
@@ -13,7 +13,10 @@ const name = "ultrasonic"
 
 type ultrasonicModule struct{}
 
-var running = false
+var (
+	running   = false
+	startTime = time.Now()
+)
 
 func Register() {
 	m := modules.Module{name, ultrasonicModule{}}
@@ -21,53 +24,53 @@ func Register() {
 }
 
 func (m ultrasonicModule) Start(c interface{}) {
-	go start()
+	running = true
+	go distance()
 }
 
 func (m ultrasonicModule) Stop() {
 	running = false
 }
 
-func start() {
-	running = true
-	for running {
-		fmt.Println(distance(17, 4))
-		time.Sleep(100 * time.Millisecond)
-	}
-}
-
-func distance(echoNum int, triggerNum int) (float64, error) {
-	fmt.Println("Messe")
-	echo, _ := embd.NewDigitalPin(echoNum)
+func distance() {
+	echo, _ := embd.NewDigitalPin(17)
 	echo.SetDirection(embd.In)
-	trigger, _ := embd.NewDigitalPin(triggerNum)
+	trigger, _ := embd.NewDigitalPin(4)
 	trigger.SetDirection(embd.Out)
 
-	// input := make(chan interface{})
-	// err := echo.Watch(embd.EdgeRising, func(echo embd.DigitalPin) {
-	// 	input <- true
-	// })
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	trigger.Write(embd.High)
-	time.Sleep(50 * time.Microsecond)
-	trigger.Write(embd.Low)
-
-	// startTime := time.Now() // Record time when ECHO goes high
-	// <-input
-	// echo.StopWatching()
-	// duration := time.Since(startTime)
-
-	duration, err := echo.TimePulse(embd.High)
+	input := make(chan int)
+	err := echo.Watch(embd.EdgeBoth, func(echo embd.DigitalPin) {
+		read, _ := echo.Read()
+		input <- read
+	})
 	if err != nil {
-		return 0, err
+		panic(err)
+	}
+	go measure(input)
+
+	for running {
+		trigger.Write(embd.High)
+		time.Sleep(50 * time.Microsecond)
+		trigger.Write(embd.Low)
+
+		time.Sleep(50 * time.Millisecond)
 	}
 
-	distance := float64(duration.Nanoseconds()) / 10000000 * 170
-
+	echo.StopWatching()
 	echo.Close()
 	trigger.Close()
-	return distance, nil
+	close(input)
+}
+
+func measure(c chan int) {
+	for {
+		val := <-c
+		if val == 1 {
+			startTime = time.Now()
+			continue
+		}
+		duration := time.Since(startTime)
+		distance := float64(duration.Nanoseconds()) / 10000000 * 171.5
+		fmt.Println(distance)
+	}
 }

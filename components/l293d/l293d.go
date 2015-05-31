658ae3148be970209741a8eed231f9c66933e0e9
psycho <priebeman@googlemail.com>
2015-05-31 19:58:07 +0200

    add motor forward + backward test

--- components/l293d/l293d.go
+++ components/l293d/l293d.go
@@ -3,10 +3,12 @@ package l293d
 import (
 	"gotank/libs/embd"
 	_ "gotank/libs/embd/host/all"
+	"time"
 )
 
 type MotorShieldL293d struct {
 	latch, clk, enable, data, pwm, motor int
+	pin                                  embd.PWMPin
 }
 
 const (
@@ -27,7 +29,7 @@ const (
 var latchState byte = 0
 
 func InitMotor(latch int, clk int, enable int, data int, pwm int, motor int) *MotorShieldL293d {
-	m := MotorShieldL293d{latch, clk, enable, data, pwm, motor}
+	m := MotorShieldL293d{latch, clk, enable, data, pwm, motor, nil}
 	m.init()
 	return &m
 }
@@ -37,22 +39,23 @@ func (m MotorShieldL293d) init() {
 	embd.SetDirection(m.enable, embd.Out)
 	embd.SetDirection(m.data, embd.Out)
 	embd.SetDirection(m.clk, embd.Out)
+	m.pin, _ = embd.NewPWMPin(m.pwm)
 	m.latchTx()
 	embd.DigitalWrite(m.enable, embd.Low)
 }
 
 func (m MotorShieldL293d) initMotor() {
 	if m.motor == 1 {
-		latchState &= m.bv(motor1A) & m.bv(motor1B)
+		latchState &= m.bv(motor1A) ^ 255&m.bv(motor1B) ^ 255
 	}
 	if m.motor == 2 {
-		latchState &= m.bv(motor2A) & m.bv(motor2B)
+		latchState &= m.bv(motor2A) ^ 255&m.bv(motor2B) ^ 255
 	}
 	if m.motor == 3 {
-		latchState &= m.bv(motor3A) & m.bv(motor3B)
+		latchState &= m.bv(motor3A) ^ 255&m.bv(motor3B) ^ 255
 	}
 	if m.motor == 4 {
-		latchState &= m.bv(motor4A) & m.bv(motor4B)
+		latchState &= m.bv(motor4A) ^ 255&m.bv(motor4B) ^ 255
 	}
 	m.latchTx()
 	m.Speed(1023)
@@ -93,17 +96,23 @@ func (m MotorShieldL293d) command(cmd int) {
 
 func (m MotorShieldL293d) latchTx() {
 	embd.DigitalWrite(m.latch, embd.Low)
+	time.Sleep(100 * time.Nanosecond)
 	embd.DigitalWrite(m.data, embd.Low)
+	time.Sleep(100 * time.Nanosecond)
 	var i byte
 	for i = 0; i < 8; i++ {
 		embd.DigitalWrite(m.clk, embd.Low)
+		time.Sleep(100 * time.Nanosecond)
 		if latchState&m.bv(7-i) > 0 {
 			embd.DigitalWrite(m.data, embd.High)
 		} else {
 			embd.DigitalWrite(m.data, embd.Low)
 		}
+		time.Sleep(100 * time.Nanosecond)
 		embd.DigitalWrite(m.clk, embd.High)
+		time.Sleep(100 * time.Nanosecond)
 	}
+	time.Sleep(100 * time.Nanosecond)
 	embd.DigitalWrite(m.latch, embd.High)
 }
 
@@ -120,9 +129,13 @@ func (m MotorShieldL293d) Backward() {
 }
 
 func (m MotorShieldL293d) Speed(i int) {
-
+	m.pin.SetDuty(i)
 }
 
 func (m MotorShieldL293d) Stop() {
 	m.command(release)
+}
+
+func (m MotorShieldL293d) Release() {
+	m.pin.Close()
 }
--- modules/motorshield/motorshield.go
+++ modules/motorshield/motorshield.go
@@ -1,13 +1,17 @@
 package motorshield
 
 import (
-	_ "gotank/components/l293d"
+	"fmt"
+	"gotank/components/l293d"
 	"gotank/modules"
+	"time"
 )
 
 const name = "motorshield"
 
 type motorshieldModule struct{}
+
+var running = false
 
 func Register() {
 	m := modules.Module{name, motorshieldModule{}}
@@ -15,7 +19,23 @@ func Register() {
 }
 
 func (m motorshieldModule) Start(c interface{}) {
+	go startMotor()
 }
 
 func (m motorshieldModule) Stop() {
+	running = false
+}
+
+func startMotor() {
+	fmt.Println("Motor Start")
+	running = true
+	motor := l293d.InitMotor(9, 25, 22, 23, 18, 3)
+	// latch int, clk int, enable int, data int, pwm int, motor int
+	motor.Forward()
+	time.Sleep(1 * time.Second)
+	motor.Backward()
+	time.Sleep(1 * time.Second)
+	motor.Stop()
+	fmt.Println("Motor Stop")
+	time.Sleep(10 * time.Second)
 }

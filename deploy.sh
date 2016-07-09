227d1aa6bdb7c21fc837859d8d25b3b980934b50
psycho <priebeman@googlemail.com>
2016-07-09 08:21:12 +0200

    Change ip

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 go generate
 GOOS=linux GOARCH=arm GOARM=6 go build
 #ssh -l pi 192.168.0.226 "sudo killall gotank && cd ~/gotank && ./reset.sh"
-scp -r gotank pi@192.168.0.212:~/gotank/
+scp -r gotank pi@192.168.0.208:~/gotank/
 #scp ./config/*.yml pi@192.168.0.226:~/gotank/config/
 #ssh -l pi 192.168.0.226 "cd ~/gotank && ./reset.sh"
 #ssh -n -f -l pi 192.168.0.226 "sh -c 'cd ~/gotank; sudo nohup ./gotank > /dev/null 2>&1 &'"
